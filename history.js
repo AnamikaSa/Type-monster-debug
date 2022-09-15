fd438a4f17eeebe9ca1ccab2b6828e00420f1f93
AnamikaSa <amanika0061@gmail.com>
2022-09-15 22:09:00 +0600

    blog part added

--- history.js
+++ history.js
@@ -1,13 +1,13 @@
 const histories = document.getElementById("histories");
 
-function addHistory(questionText, timeTakenRound, errorCount) {
+function addHistory(questionText, timeTaken, errorCount) {
   const newRow = document.createElement("div");
   newRow.classList.add("card");
 
   newRow.innerHTML = `
   <h3>${questionText}</h3>
   <div>
-  <p>  You took: <span class="bold">${timeTakenRound}</span> seconds</p>
+  <p>  You took: <span class="bold">${timeTaken}</span> seconds</p>
   <p>  You made <span class="bold red">${errorCount}</span> mistakes</p>
   </div>
   `;
@@ -15,7 +15,7 @@ function addHistory(questionText, timeTakenRound, errorCount) {
   histories.appendChild(newRow);
 
   let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
-  previousTests.push({ questionText, timeTakenRound, errorCount });
+  previousTests.push({ questionText, timeTaken, errorCount });
   localStorage.setItem("testHistory", JSON.stringify(previousTests));
 
   displayHistory();
@@ -36,4 +36,8 @@ function displayHistory() {
 
     histories.appendChild(newRow);
   });
+}
+
+function deleteItems() {
+  localStorage.clear();
 }
